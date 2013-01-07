3aa4ce326fb1283834ae86dd65ac0d179724c482
Evgeny Shishkin <johnkil78@gmail.com>
2013-01-07 23:55:27 +0400

    formatting of the source code

--- library/src/com/devspark/appmsg/MsgManager.java
+++ library/src/com/devspark/appmsg/MsgManager.java
@@ -27,147 +27,145 @@ import android.view.animation.AnimationUtils;
 
 /**
  * 
- * @author e.shishkin
- *
+ * @author Evgeny Shishkin
+ * 
  */
 class MsgManager extends Handler {
 
-	private static final int MESSAGE_DISPLAY = 0xc2007;
-	private static final int MESSAGE_ADD_VIEW = 0xc20074dd;
-	private static final int MESSAGE_REMOVE = 0xc2007de1;
-	
-	private static MsgManager mInstance;
+    private static final int MESSAGE_DISPLAY = 0xc2007;
+    private static final int MESSAGE_ADD_VIEW = 0xc20074dd;
+    private static final int MESSAGE_REMOVE = 0xc2007de1;
 
-	private Queue<AppMsg> msgQueue;
-	private Animation inAnimation, outAnimation;
+    private static MsgManager mInstance;
 
-	private MsgManager() {
-		msgQueue = new LinkedList<AppMsg>();
-	}
+    private Queue<AppMsg> msgQueue;
+    private Animation inAnimation, outAnimation;
 
-	/**
-	 * @return The currently used instance of the {@link MsgManager}.
-	 */
-	static synchronized MsgManager getInstance() {
-		if (mInstance == null) {
-			mInstance = new MsgManager();
-		}
-		return mInstance;
-	}
+    private MsgManager() {
+        msgQueue = new LinkedList<AppMsg>();
+    }
 
-	/**
-	 * Inserts a {@link AppMsg} to be displayed.
-	 * 
-	 * @param AppMsg
-	 */
-	void add(AppMsg appMsg) {
-		msgQueue.add(appMsg);
-		if (inAnimation == null) {
-			inAnimation = AnimationUtils.loadAnimation(appMsg.getActivity(),
-					android.R.anim.fade_in);
-		}
-		if (outAnimation == null) {
-			outAnimation = AnimationUtils.loadAnimation(appMsg.getActivity(),
-					android.R.anim.fade_out);
-		}
-		displayMsg();
-	}
-	
-	/**
-	 * Removes all {@link AppMsg} from the queue.
-	 */
-	void clearMsg(AppMsg appMsg) {
-		msgQueue.remove(appMsg);
-	}
+    /**
+     * @return The currently used instance of the {@link MsgManager}.
+     */
+    static synchronized MsgManager getInstance() {
+        if (mInstance == null) {
+            mInstance = new MsgManager();
+        }
+        return mInstance;
+    }
 
-	/**
-	 * Removes all {@link AppMsg} from the queue.
-	 */
-	void clearAllMsg() {
-		if (msgQueue != null) {
-			msgQueue.clear();
-		}
-		removeMessages(MESSAGE_DISPLAY);
-		removeMessages(MESSAGE_ADD_VIEW);
-		removeMessages(MESSAGE_REMOVE);
-	}
+    /**
+     * Inserts a {@link AppMsg} to be displayed.
+     * 
+     * @param AppMsg
+     */
+    void add(AppMsg appMsg) {
+        msgQueue.add(appMsg);
+        if (inAnimation == null) {
+            inAnimation = AnimationUtils.loadAnimation(appMsg.getActivity(),
+                    android.R.anim.fade_in);
+        }
+        if (outAnimation == null) {
+            outAnimation = AnimationUtils.loadAnimation(appMsg.getActivity(),
+                    android.R.anim.fade_out);
+        }
+        displayMsg();
+    }
 
-	/**
-	 * Displays the next {@link AppMsg} within the queue.
-	 */
-	private void displayMsg() {
-		if (msgQueue.isEmpty()) {
-			return;
-		}
-		// First peek whether the AppMsg is being displayed.
-		final AppMsg appMsg = msgQueue.peek();
-		// If the activity is null we throw away the AppMsg.
-		if (appMsg.getActivity() == null) {
-			msgQueue.poll();
-		}
-		final Message msg;
-		if (!appMsg.isShowing()) {
-			// Display the AppMsg
-			msg = obtainMessage(MESSAGE_ADD_VIEW);
-			msg.obj = appMsg;
-			sendMessage(msg);
-		} else {
-			msg = obtainMessage(MESSAGE_DISPLAY);
-			sendMessageDelayed(msg, appMsg.getDuration()
-					+ inAnimation.getDuration() + outAnimation.getDuration());
-		}
-	}
+    /**
+     * Removes all {@link AppMsg} from the queue.
+     */
+    void clearMsg(AppMsg appMsg) {
+        msgQueue.remove(appMsg);
+    }
 
-	/**
-	 * Removes the {@link AppMsg}'s view after it's display duration.
-	 * 
-	 * @param appMsg
-	 *            The {@link AppMsg} added to a {@link ViewGroup} and should be
-	 *            removed.s
-	 */
-	private void removeMsg(final AppMsg appMsg) {
-		ViewGroup parent = ((ViewGroup) appMsg.getView().getParent());
-		if (parent != null) {
-			appMsg.getView().startAnimation(outAnimation);
-			// Remove the AppMsg from the queue.
-			msgQueue.poll();
-			// Remove the AppMsg from the view's parent.
-			parent.removeView(appMsg.getView());
-			Message msg = obtainMessage(MESSAGE_DISPLAY);
-			sendMessage(msg);
-		}
-	}
+    /**
+     * Removes all {@link AppMsg} from the queue.
+     */
+    void clearAllMsg() {
+        if (msgQueue != null) {
+            msgQueue.clear();
+        }
+        removeMessages(MESSAGE_DISPLAY);
+        removeMessages(MESSAGE_ADD_VIEW);
+        removeMessages(MESSAGE_REMOVE);
+    }
 
-	private void addMsgToView(AppMsg appMsg) {
-		if (appMsg.getView().getParent() == null) {
-			appMsg.getActivity().addContentView(
-					appMsg.getView(),
-					appMsg.getLayoutParams());
-		}
-		appMsg.getView().startAnimation(inAnimation);
-		final Message msg = obtainMessage(MESSAGE_REMOVE);
-		msg.obj = appMsg;
-		sendMessageDelayed(msg, appMsg.getDuration());
-	}
+    /**
+     * Displays the next {@link AppMsg} within the queue.
+     */
+    private void displayMsg() {
+        if (msgQueue.isEmpty()) {
+            return;
+        }
+        // First peek whether the AppMsg is being displayed.
+        final AppMsg appMsg = msgQueue.peek();
+        // If the activity is null we throw away the AppMsg.
+        if (appMsg.getActivity() == null) {
+            msgQueue.poll();
+        }
+        final Message msg;
+        if (!appMsg.isShowing()) {
+            // Display the AppMsg
+            msg = obtainMessage(MESSAGE_ADD_VIEW);
+            msg.obj = appMsg;
+            sendMessage(msg);
+        } else {
+            msg = obtainMessage(MESSAGE_DISPLAY);
+            sendMessageDelayed(msg, appMsg.getDuration()
+                    + inAnimation.getDuration() + outAnimation.getDuration());
+        }
+    }
 
-	@Override
-	public void handleMessage(Message msg) {
-		final AppMsg appMsg;
-		switch (msg.what) {
-		case MESSAGE_DISPLAY:
-			displayMsg();
-			break;
-		case MESSAGE_ADD_VIEW:
-			appMsg = (AppMsg) msg.obj;
-			addMsgToView(appMsg);
-			break;
-		case MESSAGE_REMOVE:
-			appMsg = (AppMsg) msg.obj;
-			removeMsg(appMsg);
-			break;
-		default:
-			super.handleMessage(msg);
-			break;
-		}
-	}
+    /**
+     * Removes the {@link AppMsg}'s view after it's display duration.
+     * 
+     * @param appMsg The {@link AppMsg} added to a {@link ViewGroup} and should be removed.s
+     */
+    private void removeMsg(final AppMsg appMsg) {
+        ViewGroup parent = ((ViewGroup) appMsg.getView().getParent());
+        if (parent != null) {
+            appMsg.getView().startAnimation(outAnimation);
+            // Remove the AppMsg from the queue.
+            msgQueue.poll();
+            // Remove the AppMsg from the view's parent.
+            parent.removeView(appMsg.getView());
+            Message msg = obtainMessage(MESSAGE_DISPLAY);
+            sendMessage(msg);
+        }
+    }
+
+    private void addMsgToView(AppMsg appMsg) {
+        if (appMsg.getView().getParent() == null) {
+            appMsg.getActivity().addContentView(
+                    appMsg.getView(),
+                    appMsg.getLayoutParams());
+        }
+        appMsg.getView().startAnimation(inAnimation);
+        final Message msg = obtainMessage(MESSAGE_REMOVE);
+        msg.obj = appMsg;
+        sendMessageDelayed(msg, appMsg.getDuration());
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+        final AppMsg appMsg;
+        switch (msg.what) {
+            case MESSAGE_DISPLAY:
+                displayMsg();
+                break;
+            case MESSAGE_ADD_VIEW:
+                appMsg = (AppMsg) msg.obj;
+                addMsgToView(appMsg);
+                break;
+            case MESSAGE_REMOVE:
+                appMsg = (AppMsg) msg.obj;
+                removeMsg(appMsg);
+                break;
+            default:
+                super.handleMessage(msg);
+                break;
+        }
+    }
 }
--- sample/src/com/devspark/appmsg/sample/MainActivity.java
+++ sample/src/com/devspark/appmsg/sample/MainActivity.java
@@ -28,49 +28,49 @@ import com.devspark.appmsg.AppMsg;
 /**
  * Sample of AppMsg library.
  * 
- * @author e.shishkin
+ * @author Evgeny Shishkin
  * 
  */
 public class MainActivity extends SherlockActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-	/**
-	 * Button onClick listener.
-	 * 
-	 * @param v
-	 */
-	public void showAppMsg(View v) {
-		final CharSequence msg = ((Button) v).getText();
-		final AppMsg.Style style;
-		switch (v.getId()) {
-		case R.id.alert:
-			style = AppMsg.STYLE_ALERT;
-			break;
-		case R.id.confirm:
-			style = AppMsg.STYLE_CONFIRM;
-			break;
-		case R.id.info:
-			style = AppMsg.STYLE_INFO;
-			break;
-		case R.id.custom:
-			style = new AppMsg.Style(AppMsg.LENGTH_SHORT, R.color.custom);
-			break;
+    /**
+     * Button onClick listener.
+     * 
+     * @param v
+     */
+    public void showAppMsg(View v) {
+        final CharSequence msg = ((Button) v).getText();
+        final AppMsg.Style style;
+        switch (v.getId()) {
+            case R.id.alert:
+                style = AppMsg.STYLE_ALERT;
+                break;
+            case R.id.confirm:
+                style = AppMsg.STYLE_CONFIRM;
+                break;
+            case R.id.info:
+                style = AppMsg.STYLE_INFO;
+                break;
+            case R.id.custom:
+                style = new AppMsg.Style(AppMsg.LENGTH_SHORT, R.color.custom);
+                break;
 
-		default:
-			return;
-		}
+            default:
+                return;
+        }
 
-		// create {@link AppMsg} with specify type
-		AppMsg appMsg = AppMsg.makeText(this, msg, style);
-		if (((CheckBox)(findViewById(R.id.bottom))).isChecked()) {
-			appMsg.setLayoutGravity(Gravity.BOTTOM);
-		}
-		appMsg.show();
-	}
+        // create {@link AppMsg} with specify type
+        AppMsg appMsg = AppMsg.makeText(this, msg, style);
+        if (((CheckBox) (findViewById(R.id.bottom))).isChecked()) {
+            appMsg.setLayoutGravity(Gravity.BOTTOM);
+        }
+        appMsg.show();
+    }
 
 }
